5e1255dc142544c5009aaa9f0cc34c623909a9e1
Frank Gerlach <frankgerlach.tai@gmx.de>
2017-07-06 20:03:01 +0200

    erste

---
+++ HashAdapter.h
@@ -0,0 +1,36 @@
+#ifndef HASH_ADAPTER
+#define HASH_ADAPTER
+
+/* Adapter fuer verschiedene Schluessel vom Typ Ganzzahl
+ 
+   (C) Frank Gerlach 2017
+
+   Nichtkommerzielle Nutzung ist lizenz- und kostenfrei.
+
+   Kommerzielle Nutzung erfordert eine kostenpflichtige Lizenz.
+   Nutzungsplaene fangen an ab 150 Euro pro Produkt.
+ 
+   Einzelheiten bitte erfragen bei frankgerlach.tai@gmx.de
+
+*/
+
+/* Adapter-Klasse fuer Schluessel vom Typ Ganzzahl */
+class SchlAdapterInt
+{
+public:
+   static bool gleich(const uint64_t a,const uint64_t b)
+   {
+     return a == b;
+   }
+
+   static uint64_t randomisiere(const uint64_t schluessel)
+   {
+      return schluessel*98487831+83847871;
+   }
+   static void loesche(uint64_t& schluessel)
+   {
+      schluessel = 0;
+   }
+};
+
+#endif
---
+++ HashAdapterZK.h
@@ -0,0 +1,36 @@
+#ifndef HASH_ADAPTER_ZK
+#define HASH_ADAPTER_ZK
+
+#include "zk.h"
+#include "SuchoiHash.h"
+
+/* Adapter-Klasse fuer Schluessel vom Typ Zeichenkette */
+class SchlAdapterZK
+{
+public:
+   static bool gleich(const Zeichenkette& a,const Zeichenkette& b)
+   {
+      return a == b;
+   }
+
+   static uint64_t randomisiere(const Zeichenkette& schluessel)
+   {
+      return suchoi(schluessel.zk(),schluessel.laenge());
+   }
+   static void loesche(Zeichenkette& schluessel )
+   {
+      schluessel.leere(true);
+   }
+};
+
+/* Adapter-Klasse fuer Wert-Objekte vom Typ Zeichenkette */
+class WertAdapterZK
+{
+public:
+  static void loesche(Zeichenkette& wert)
+  {
+      wert.leere(true);
+  }
+};
+
+#endif
