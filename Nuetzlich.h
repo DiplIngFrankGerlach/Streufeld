76814e96aa99f899abb6122ec016a4e77247b7fe
Frank Gerlach <frankgerlach.tai@gmx.de>
2017-07-19 22:09:11 +0200

    Kleine Erweiterung

--- Nuetzlich.h
+++ Nuetzlich.h
@@ -8,4 +8,10 @@ uint64_t naechstGroessereZweierPotenz(uint64_t zahl);
 
 bool leseZahl(Zeichenkette& zk,uint64_t& zahl);
 
+bool istNormalZeichen(char zeichen);
+
+bool istBst(char zeichen);
+
+bool istKopfzeilenWertZeichen(char zeichen);
+
 #endif
